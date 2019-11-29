9ec953145f6971aea7053d66425ee770c5a5ff1b
caioAugustoRodrigues <caio.turmaxii@gmail.com>
2019-11-29 16:59:56 -0300

    master

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -5,7 +5,7 @@ import '../../css/main.css'
 function Hero() {
   return (
       <div className="hero text-center">
-        <h1>Your new home is Here.</h1>
+        <h1 className="shadow-sm">Your new home is Here.</h1>
         <p>We will help you find the best prices in your region</p>
       </div>
   )
